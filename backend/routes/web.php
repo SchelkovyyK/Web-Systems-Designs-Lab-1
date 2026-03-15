2dcc90c2532b3666854d988f7220624a9dfb0001
Kyrylo <kirillschelk1@gmail.com>
2026-03-15 21:19:07 +0100

    fixing errors

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome'); 
+});
+
+Route::get('/health', function () {
+    return response()->json([
+        'status' => 'ok',
+        'service' => 'backend',
+    ]);
 });
