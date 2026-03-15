193363dbd24a813e12e8cc379eb7d74c3dbcfef7
Kyrylo <kirillschelk1@gmail.com>
2026-03-15 20:54:03 +0100

    Project 1: scaffold Laravel backend and Vue frontend

---
+++ frontend/App/src/components/TheWelcome.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+
+const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Climate control project</template>
+
+    <a
+      href="https://github.com/SchelkovyyK/Climate-control-Iot_Project"
+      target="_blank"
+      rel="noopener"
+      >Climate-control-Iot_Project</a
+    >
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>DocuSynth</template>
+
+    <a href="https://github.com/SchelkovyyK/DocuSynth" target="_blank" rel="noopener">DocuSynth</a>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Fake-Detect-Project</template>
+
+    <a href="https://github.com/SchelkovyyK/Fake-Detect-Project" target="_blank" rel="noopener"
+      >Fake-Detect-Project</a
+    >
+  </WelcomeItem>
+</template>
---
+++ frontend/App/src/views/AboutView.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="about">
+    <swiper
+      :style="{
+        '--swiper-navigation-color': '#fff',
+        '--swiper-pagination-color': '#fff',
+      }"
+      :loop="true"
+      :spaceBetween="10"
+      :navigation="true"
+      :thumbs="{ swiper: thumbsSwiper }"
+      :modules="modules"
+      class="mySwiper2"
+    >
+      <swiper-slide
+        ><img src="../../public/images/dfb175aafbd34ff2d5c5306d8421c69a.jpg"
+      /></swiper-slide>
+      <swiper-slide
+        ><img src="../../public/images/704cac241c6ab84d1fce9e4a76e00226.jpg"
+      /></swiper-slide>
+      <swiper-slide
+        ><img src="../../public/images/924192b2cdbec6802e7fe4229e2e1bd9.jpg"
+      /></swiper-slide>
+
+      <swiper-slide
+        ><img src="../../public/images/7c671e59434500a7bbce783166ea31e5.jpg"
+      /></swiper-slide>
+      <swiper-slide
+        ><img src="../../public/images/b42e614551ef0cd1d8f9e67914cd8d64.jpg"
+      /></swiper-slide>
+      <swiper-slide
+        ><img src="../../public/images/87051939c904c85d4244237993cedb13.jpg"
+      /></swiper-slide>
+    </swiper>
+
+    <swiper
+      @swiper="setThumbsSwiper"
+      :loop="true"
+      :spaceBetween="10"
+      :slidesPerView="3"
+      :freeMode="true"
+      :watchSlidesProgress="true"
+      :modules="modules"
+      class="mySwiper"
+    >
+      <swiper-slide
+        ><img src="../../public/images/dfb175aafbd34ff2d5c5306d8421c69a.jpg"
+      /></swiper-slide>
+      <swiper-slide
+        ><img src="../../public/images/704cac241c6ab84d1fce9e4a76e00226.jpg"
+      /></swiper-slide>
+      <swiper-slide
+        ><img src="../../public/images/924192b2cdbec6802e7fe4229e2e1bd9.jpg"
+      /></swiper-slide>
+
+      <swiper-slide
+        ><img src="../../public/images/7c671e59434500a7bbce783166ea31e5.jpg"
+      /></swiper-slide>
+      <swiper-slide
+        ><img src="../../public/images/b42e614551ef0cd1d8f9e67914cd8d64.jpg"
+      /></swiper-slide>
+      <swiper-slide
+        ><img src="../../public/images/87051939c904c85d4244237993cedb13.jpg"
+      /></swiper-slide>
+    </swiper>
+  </div>
+</template>
+<script>
+import { ref } from 'vue'
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue'
+
+// Import Swiper styles
+import 'swiper/css'
+
+import 'swiper/css/free-mode'
+import 'swiper/css/navigation'
+import 'swiper/css/thumbs'
+
+import '../assets/main.css'
+
+// import required modules
+import { FreeMode, Navigation, Thumbs } from 'swiper/modules'
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    const thumbsSwiper = ref(null)
+
+    const setThumbsSwiper = (swiper) => {
+      thumbsSwiper.value = swiper
+    }
+
+    return {
+      thumbsSwiper,
+      setThumbsSwiper,
+      modules: [FreeMode, Navigation, Thumbs],
+    }
+  },
+}
+</script>
+
+<style>
+.about {
+  width: 50vw;
+  height: 90vh;
+
+  display: flex;
+  flex-direction: column;
+}
+
+@media (min-width: 1024px) {
+  .about {
+    min-height: 10vh;
+    align-items: center;
+  }
+}
+</style>
